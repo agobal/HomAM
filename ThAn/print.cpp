162d97d06ca8fe2430d015ee8f8a31a42bf4bd53
agobal <agobal@ucdavis.edu>
2017-02-06 17:50:47 -0800

    writing to text file

--- ThAn/print.cpp
+++ ThAn/print.cpp
@@ -4,16 +4,27 @@
 #include <boost/random/normal_distribution.hpp>
 #include <boost/tuple/tuple.hpp>
 #include <boost/tuple/tuple_io.hpp>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
 // Utility method to print adjacency matrix
 void PowBed::print()
 {
-   for (int u=0; u<PP.particle_count; u++)
-   {
-      // for (int v=0; v<PP.particle_count; v++)
-         cout << PP.r_p[u] << " ";
-      cout << endl;
-   }
+	ofstream xfile("x.txt");
+	for (int i = 0; i < PP.grid_count; ++i)
+	{
+		for (int j = 0; j < PP.particle_count; ++j)
+		{
+			xfile << PP.x_p[i][j];
+			xfile << " ";
+		}
+		xfile << "\n";
+	}
+	xfile.close();
+   // for (int u=0; u<PP.particle_count; u++)
+   // {
+   //   cout << PP.r_p[u] << " ";
+   // }
 }
